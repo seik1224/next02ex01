dacb94eaa2faff22e138a7c38df8321efb0cfa0c
박세익 <seik1224@naver.com>
2024-10-25 14:22:25 +0900

    넥스트 2회차 실습1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import Section01 from "./(route)/Section01";
-import Section02 from "./(route)/Section02";
+import Section02 from "./(route)/Section02js";
 import Section03 from "./(route)/Section03";
 import Section04 from "./(route)/Section04";
 
